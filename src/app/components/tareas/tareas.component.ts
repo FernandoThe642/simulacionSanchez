af82c31f43476a9c674179cdc2bdf51b01a06955
jsanchez <jsanchezr6@est.ups.edu.ec>
2024-10-23 08:42:35 -0500

    falta conexión con servicio

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,11 +1,13 @@
 
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TareaService } from '../../services/tarea.service';
 
 @Component({
   selector: 'app-tareas',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './tareas.component.html',
   styleUrl: './tareas.component.scss'
 })
@@ -13,8 +15,29 @@ export class TareasComponent {
 
   nombreTarea: string = '';
   descripcionTarea: string = '';
-  
+  tareas: any;   
+
+  constructor(private tareasService: TareaService){
+
+  }
+
   agregarTarea() {
+    if(this.validarFormulario()){
+      const tarea = {
+        nombreTarea: this.nombreTarea,
+        descripcionTarea: this.descripcionTarea
+      };
+    }
+    
     
   }
+
+  ngOnInit(): void {
+    this.tareasService.guardarTarea()
+  }
+
+    // Validar el formulario
+    validarFormulario(): boolean {
+      return this.nombreTarea !== '' && this.descripcionTarea !== '';
+    }
 }
--- src/app/services/tarea.service.ts
+++ src/app/services/tarea.service.ts
@@ -4,6 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TareaService {
+  
+  tareas: any =[]
 
   constructor() { }
+
+  guardarTarea(tarea: any) {
+    let tareas = JSON.parse(localStorage.getItem('tareas') || '[]');
+    tareas.push(tarea);
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }
 }
